5814753fe750cdac229b73247d07f4c882b8549c
KristinLind <kristin.lind@comcast.net>
2025-05-21 11:57:49 -0600

    icon

--- scripts/place.js
+++ scripts/place.js
@@ -1,17 +1,9 @@
 const temperature = 47;
 const windSpeed = 10;
 
-document.getElementById("temperature").textContent = temperature;
-document.getElementById("windSpeed").textContent = windSpeed;
-
-const windChill = (temperature <= 50 && windSpeed > 3)
-    ? calculateWindChill(temperature, windSpeed)
-    : "N/A";
-
-document.getElementById("windChill").textContent = windChill;
-
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementByIdd("lastModified").textContent = document.lastModified;
+document.getElementById("temperature").textContent = `${temperature}°F`;
+document.getElementById("conditions").textContent = "Partly Cloudy";
+document.getElementById("windSpeed").textContent = 10;
 
 function calculateWindChill(tempF, speedMph) {
     return (
@@ -21,3 +13,12 @@ function calculateWindChill(tempF, speedMph) {
         0.4275 * tempF * Math.pow(speedMph, 0.16)
     ).toFixed(1);
 }
+
+const windChill = (temperature <= 50 && windSpeed > 3)
+    ? calculateWindChill(temperature, windSpeed)
+    : "N/A";
+
+document.getElementById("windChill").textContent = windChill;
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
