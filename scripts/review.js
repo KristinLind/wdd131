40789edaf8d3c60b0b2ce784a28461c6e287e11c
KristinLind <kristin.lind@comcast.net>
2025-06-09 15:13:51 -0600

    review

--- scripts/review.js
+++ scripts/review.js
@@ -1,7 +1,15 @@
-document.addEventListener("DOMCountentLoaded", () => {
-    const reviewCount = localStorage.getItem("reviewCount") || 0;
-    const display = document.getElementById("reviewCountDisplay");
+document.addEventListener("DOMContentLoaded", () => {
+    const count = localStorage.getItem("reviewCount") || 0;
+    const plural = count == 1 ? "review" : "reviews";
+    const display = document.getElementById("reviewCount");
+  
     if (display) {
-        display.textContent = reviewCount;
+      display.textContent = `You have submitted ${count} ${plural}.`;
+    }
+  
+    const lastModified = document.getElementById("lastModified");
+    if (lastModified) {
+      lastModified.textContent = document.lastModified;
     }
 });
+  
