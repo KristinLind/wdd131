19b0822a092bfedef322ea65e491a11cff6422e4
KristinLind <kristin.lind@comcast.net>
2025-06-13 14:04:13 -0600

    js

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -8,8 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
     const yearSpan = document.querySelector('#year');
-    if (yearSpan) yearSpan.textContent = new Date().getFullYear();
-  
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
     const modifiedSpan = document.querySelector('#lastModified');
     if (modifiedSpan) modifiedSpan.textContent = document.lastModified;
   
